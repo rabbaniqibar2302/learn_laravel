b38edc560f6f8bfc98d99ec43da3d468d4efbb84
rizkiakbar2302 <rizkiakbar2302@gmail.com>
2021-02-01 07:41:31 +0700

    third

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,6 @@
 @section('content')
     @foreach ($articles as $article)
         <p>{{ $article->title }}</p>
+        
     @endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,3 @@
 
 
 Route::get('/', 'HomeController@index')->name('home');
-
-Route::get('/about' , 'PageController@about')->name('pages.about');
-
-Route::get('/contact' , 'PageController@contact')->name('pages.contact');
